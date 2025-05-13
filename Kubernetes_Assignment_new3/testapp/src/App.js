62d6df013b3dbecfe12a541f92603ed6f3afe56c
ShivaChouhan <shivanandchouhan2002@gmail.com>
2025-05-13 01:51:53 +0000

    react_update

--- Kubernetes_Assignment_new3/testapp/src/App.js
+++ Kubernetes_Assignment_new3/testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Update_1 react_02
+          Test Update_1 ract_02
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
