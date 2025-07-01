076872d6b933be99ca01db785183e096eb74d3ab
Shivanand Chouhan <shivanand.chouhan@calsoftinc.com>
2025-07-01 10:54:53 +0530

    new

--- Jenkins_Assignment_7/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
+++ Jenkins_Assignment_7/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello Everyone how are you ."; // Chatnged to a more friendly greeting
+        return "H ."; // Chatnged to a more friendly greeting
     }
 }
