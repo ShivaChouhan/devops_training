e90cf8e5ac4498c623a066c445dd5ebf801515d7
ShivaChouhan <shivanandchouhan2002@gmail.com>
2025-07-08 06:27:59 +0000

    sonar_new

---
+++ SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.helloapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello this is my java application."; // Changed to a more friendly greeting
+    }
+}
