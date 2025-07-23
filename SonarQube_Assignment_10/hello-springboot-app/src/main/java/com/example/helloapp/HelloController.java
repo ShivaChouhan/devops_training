61688c1c81134e878c6c73d7158aaf0c1c267028
ShivaChouhan <shivanandchouhan2002@gmail.com>
2025-07-23 05:53:33 +0000

    new

--- SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
+++ SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -1,9 +1,6 @@
 package com.example.helloapp;
 
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,10 +12,10 @@ public class HelloController {
     }
 
     // 🚨 Vulnerable endpoint (for testing CodeQL)
-    @GetMapping("/run")
-    public String runCommand(@RequestParam String cmd) throws IOException {
-        // ⚠️ CodeQL will flag this as a security issue (command injection)
-        Runtime.getRuntime().exec(cmd);
-        return "Command executed: " + cmd;
-    }
+    // @GetMapping("/run")
+    // public String runCommand(@RequestParam String cmd) throws IOException {
+    //     // ⚠️ CodeQL will flag this as a security issue (command injection)
+    //     Runtime.getRuntime().exec(cmd);
+    //     return "Command executed: " + cmd;
+    // }
 }
