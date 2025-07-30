8723532c663c5248514f02ac5fe245408fdef9b2
ShivaChouhan <shivanandchouhan2002@gmail.com>
2025-07-30 05:46:57 +0000

    sonarqube

--- SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
+++ SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private static final String DUMMY_PASSWORD = "SuperSecret123!";
+    private static final String DUMMY = "SuperSecret123!";
     @GetMapping("/hello")
     public String sayHello() {
-        
+        System.out.println("Dummy  " + DUMMY); // Logging the dummy password
         return "Hello this is my java application."; // Changed to a more friendly greeting
     }
 }
