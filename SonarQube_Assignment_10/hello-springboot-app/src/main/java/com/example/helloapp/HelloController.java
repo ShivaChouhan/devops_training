dc78f49c47e81e26834d5c12961b35317afc9d60
ShivaChouhan <shivanandchouhan2002@gmail.com>
2025-08-12 03:25:13 +0000

    sonar

--- SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
+++ SonarQube_Assignment_10/hello-springboot-app/src/main/java/com/example/helloapp/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.helloapp;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
